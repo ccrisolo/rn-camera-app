d59217865d099f11ec434f42bbf2002e2b5a7840
ccrisolo <ccrisolo3@gmail.com>
2020-07-14 15:32:14 -0700

    set flashMode prop on Camera component

--- App.js
+++ App.js
@@ -1,46 +1,53 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [cameraRef, setCameraRef] = useState(null)
+  const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flashMode, setFlashMode] = useState("on");
 
-// this will get permission from user to access camera
-useEffect(() => {
+  // this will get permission from user to access camera
+  useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
-if (hasPermission === null) {
+  if (hasPermission === null) {
     return <View />;
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
 
-
   return (
     <View style={{ flex: 1 }}>
       {/* Camera component type is for selecting the type of camera, front or back, using constants provided by 
       Camera.Constants.Type.back. ref prop is set to a state so that different methods can provided by this 
       instance of camera component can be used (e.g. cameraRef.takePictureAsunc()) */}
-      <Camera style={{ flex: 1 }} type={type} ref={ref => {
-        setCameraRef(ref) ;
-  }}>
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+        ref={(ref) => {
+          setCameraRef(ref);
+        }}
+        flashMode={flashMode}
+      >
         <View
           style={{
             flex: 1,
-            backgroundColor: 'transparent',
-            justifyContent: 'flex-end'
-          }}>
+            backgroundColor: "transparent",
+            justifyContent: "flex-end",
+          }}
+        >
           <TouchableOpacity
             style={{
               flex: 0.1,
-              alignSelf: 'flex-end'
+              alignSelf: "center",
+              
             }}
             onPress={() => {
               setType(
@@ -48,33 +55,46 @@ if (hasPermission === null) {
                   ? Camera.Constants.Type.front
                   : Camera.Constants.Type.back
               );
-            }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white', }}> Flip </Text>
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{alignSelf: 'center', marginBottom: 50}} onPress={async() => {
-            if(cameraRef){
-              let photo = await cameraRef.takePictureAsync();
-              console.log('photo', photo);
-            }
-          }}>
-            <View style={{ 
-               borderWidth: 2,
-               borderRadius:"50%",
-               borderColor: 'white',
-               height: 50,
-               width:50,
-               display: 'flex',
-               justifyContent: 'center',
-               alignItems: 'center'}}
+
+          {/* code for button press */}
+          <TouchableOpacity
+            style={{ alignSelf: "center", marginBottom: 50 }}
+            onPress={async () => {
+              if (cameraRef) {
+                let photo = await cameraRef.takePictureAsync();
+                console.log("photo", photo);
+              }
+            }}
+          >
+            <View
+              style={{
+                borderWidth: 2,
+                borderRadius: "50%",
+                borderColor: "white",
+                height: 50,
+                width: 50,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
             >
-              <View style={{
-                 borderWidth: 2,
-                 borderRadius:"50%",
-                 borderColor: 'white',
-                 height: 40,
-                 width:40,
-                 backgroundColor: 'white'}} >
-              </View>
+              <View
+                style={{
+                  borderWidth: 2,
+                  borderRadius: "50%",
+                  borderColor: "white",
+                  height: 40,
+                  width: 40,
+                  backgroundColor: "white",
+                }}
+              ></View>
             </View>
           </TouchableOpacity>
         </View>
